acf3102fadfd7fdf86173440ed0738385c6256f0
Daniel Wang <danielwpz@gmail.com>
2019-10-14 18:00:50 +0800

    redis cache store

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const CacheStore = require('./lib/cache_store')
 const MemoryCacheStore = require('./lib/memory_cache_store')
+const RedisCacheStore = require('./lib/redis_cache_store')
 
 module.exports = CacheStore
 module.exports.store = {
-  MemoryCacheStore
+  MemoryCacheStore,
+  RedisCacheStore
 }
